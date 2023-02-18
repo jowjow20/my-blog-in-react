581b191c5a31220c3ba32bcc655091f2b7b61e15
jowjow20 <43030591+jowjow20@users.noreply.github.com>
2023-02-19 00:43:19 +0800

    my-blog in react

---
+++ src/components/ArticlesList.js
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+
+const ArticlesList = ({ articles }) => {
+    return (
+        <>
+            {articles.map(articles => (
+            <Link key={articles.name} className='article-list-item' to={`/articles/${articles.name}`}>
+                <h3>{articles.title}</h3>
+                <p>{articles.content[0].substring(0, 250)}...</p>
+            </Link>
+        ))}
+        </>
+    );
+}
+
+export default ArticlesList;
